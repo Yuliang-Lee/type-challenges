dac2699b9a35be16e722c2cb97110828823e6a20
xlaoyu <frank_yll@163.com>
2024-06-06 16:20:06 +0800

    feat: finish 00898

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,5 +1,9 @@
 // type Includes<T extends readonly any[], U> = NoInfer<U> extends NoInfer<T[number]> ? true : false
+type Equal<X, Y> =
+(<T>() => T extends X ? 1 : 2) extends
+(<T>() => T extends Y ? 1 : 2) ? true : false
 
-type Includes<T extends readonly any[], U> = {
-  [P in T[number]]: true
-}[U] extends true ? true : false
+type Includes<T extends readonly unknown[], U> =
+  T extends [infer First, ...infer Rest]
+    ? Equal<First, U> extends true ? true : Includes<Rest, U>
+    : false
--- summary.md
+++ summary.md
@@ -9,4 +9,4 @@
 - 00189 done, 但是看答案又更优的写法 PromiseLike 类型
 - 00268 done
 - 00533 done
-- 00898 看答案, 转成 map<value, true> 这个方法没想到
+- 00898 看答案, 改为用递归和 infer 结合一个一个对比
