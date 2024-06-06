7db38763d5bd88585149cf0849a005af388dbef9
xlaoyu <frank_yll@163.com>
2024-06-06 14:39:24 +0800

    feat: finish 00189-easy-awaited

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,3 @@
-type MyAwaited<T> = any
+type MyAwaited<T extends Promise<any> | object & { then(onfulfilled: (args: any) => any): any }> = T extends Promise<infer R> ? R extends Promise<any> ?
+  MyAwaited<R> : R
+  : T extends object & { then(onfulfilled: (args: infer F) => any): any } ? F : T
--- summary.md
+++ summary.md
@@ -6,3 +6,4 @@
 - 00014 看答案, 没实现如果是空数据返回 never, 不了解 infer 的用法, 对 extends 的使用需要加深
 - 00018 看答案, 不理解类型加 ['xxx'] 这种写法
 - 00043 看答案, 不理解 Distributive conditional types 这个概念
+- 00189 done, 但是看答案又更优的写法 PromiseLike 类型
